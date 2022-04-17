ca9efd1f699738c13459401a558956cf8a0b613c
omarfurkan <omarfurkan92@gmail.com>
2022-04-17 22:11:44 +0600

    not found page done with img

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import notFound from '../../images/not-found.jpg'
 
 const NotFound = () => {
     return (
-        <div>
-            <h2>404 component</h2>
+        <div >
+            <img className='img-fluid rounded mx-auto d-block' src={notFound} width='850px' alt="" />
         </div>
     );
 };
