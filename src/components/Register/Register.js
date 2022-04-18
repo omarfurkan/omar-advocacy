91e6fbe67c241e250d8ec29a6164a1120fc0bfb7
omarfurkan <omarfurkan92@gmail.com>
2022-04-18 22:54:08 +0600

    set regiter auth

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,12 +1,22 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import auth from '../../firebase.init'
+
 
 const Register = () => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [confirmPassword, setConfirmPassword] = useState('');
     const [error, setError] = useState('');
+    const navigate = useNavigate();
 
+    const [createUserWithEmailAndPassword, user] = useCreateUserWithEmailAndPassword(auth)
+
+
+    if (user) {
+        navigate('/');
+    }
 
 
     const handleEmailChange = event => {
@@ -19,10 +29,23 @@ const Register = () => {
         setConfirmPassword(event.target.value)
     }
 
+    const handleCreateUser = event => {
+        event.preventDefault();
+        if (password !== confirmPassword) {
+            setError('your password did not match')
+            return;
+        }
+        if (password.length < 6) {
+            setError('password must be 6 character')
+        }
+
+        createUserWithEmailAndPassword(email, password);
+    }
+
     return (
         <div className='container form-container'>
             <h2 className='text-center text-primary mb-4'>Register</h2>
-            <form >
+            <form onSubmit={handleCreateUser}>
                 <div className="form-group">
                     <label htmlFor="exampleInputEmail1">Email address</label>
                     <input onChange={handleEmailChange} type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" required />
@@ -35,6 +58,7 @@ const Register = () => {
                     <label htmlFor="exampleInputConfirmPassword1">Confirm Password</label>
                     <input onChange={handleConfirmPasswordChange} type="password" className="form-control" id="exampleInputConfirmPassword1" placeholder="Confirm Password" required />
                 </div>
+                <p className='text-danger'>{error}</p>
                 <button type="submit" className="btn btn-primary px-5">Register</button>
             </form>
             <p className='mt-2'>Already have an account? <Link className='text-primary' to='/login'>Login</Link></p>
