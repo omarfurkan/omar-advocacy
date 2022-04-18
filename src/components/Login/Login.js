a6600100456527b196c9f4f82941110e6a27cdbd
omarfurkan <omarfurkan92@gmail.com>
2022-04-18 23:16:22 +0600

    set auth in login form with redirect in home page

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,20 +1,46 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import { Link, useNavigate } from 'react-router-dom';
+import auth from '../../firebase.init';
 import './Login.css'
 
 const Login = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [signInWithEmailAndPassword, user, loading, error] = useSignInWithEmailAndPassword(auth);
+    const navigate = useNavigate();
+
+
+    if (user) {
+        navigate('/');
+    }
+
+    const handleEmailChange = event => {
+        setEmail(event.target.value);
+    }
+    const handlePasswordChange = event => {
+        setPassword(event.target.value);
+    }
+    const handleUserSignIn = event => {
+        event.preventDefault();
+        signInWithEmailAndPassword(email, password)
+    }
+
+
+
     return (
         <div className='container form-container'>
             <h2 className='text-center text-primary mb-4'>Login</h2>
-            <form >
+            <form onSubmit={handleUserSignIn}>
                 <div className="form-group">
                     <label htmlFor="exampleInputEmail1">Email address</label>
-                    <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" required />
+                    <input onChange={handleEmailChange} type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" required />
                 </div>
                 <div className="form-group mb-3">
                     <label htmlFor="exampleInputPassword1">Password</label>
-                    <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password" required />
+                    <input onChange={handlePasswordChange} type="password" className="form-control" id="exampleInputPassword1" placeholder="Password" required />
                 </div>
+                <p className='text-danger'>{error?.message}</p>
                 <button type="submit" className="btn btn-primary px-5">Login</button>
             </form>
             <p className='mt-2'>New at omarAdvocacy? <Link className='text-primary' to='/register'>Create an account</Link></p>
