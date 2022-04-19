4d06dc805ba9579cd26c008ae10231920d0c9797
omarfurkan <omarfurkan92@gmail.com>
2022-04-19 15:28:52 +0600

    set logout btn with functionality

--- src/components/Shared/Header/Header.js
+++ src/components/Shared/Header/Header.js
@@ -1,8 +1,17 @@
+import { signOut } from 'firebase/auth';
 import React from 'react';
 import { Container, Nav, Navbar } from 'react-bootstrap';
+import { useAuthState } from 'react-firebase-hooks/auth';
 import { Link } from 'react-router-dom';
+import auth from '../../../firebase.init';
 
 const Header = () => {
+    const [user] = useAuthState(auth);
+
+    const handleSignOut = () => {
+        signOut(auth)
+    }
+
     return (
         <div>
             <Navbar fixed='top' bg="light" expand="lg">
@@ -14,7 +23,11 @@ const Header = () => {
                             <Nav.Link as={Link} to="/">Home</Nav.Link>
                             <Nav.Link as={Link} to="/blog">Blog</Nav.Link>
                             <Nav.Link as={Link} to="/about">About</Nav.Link>
-                            <Nav.Link as={Link} to="/login">Login</Nav.Link>
+                            {
+                                user ?
+                                    <button style={{ 'textDecoration': 'none' }} className='btn btn-link text-dark' onClick={handleSignOut}>Sign out</button> :
+                                    <Nav.Link as={Link} to="/login">Login</Nav.Link>
+                            }
                         </Nav>
                     </Navbar.Collapse>
                 </Container>
